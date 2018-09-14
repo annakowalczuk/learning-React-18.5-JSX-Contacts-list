250719014ffd8ff9f52be4b6e8cf78fe35dfd1ac
Anna Kowalczuk <anna@kowalczuk.eu>
2018-09-14 23:19:53 +0200

    Adjust Contact.js to JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,16 +5,13 @@ var Contact = React.createClass({
   
     render: function() {
       return (
-        React.createElement('li', {className: 'contactItem'},
-          React.createElement('i', {
-            className: 'fas fa-user-circle'}),
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-          )
-        )
+        <li className="contactItem">
+          <i className="fas fa-user-circle" />
+          <p className="contactLabel">Imię: {this.props.item.firstName}</p>
+          <p className="contactLabel">Nazwisko: {this.props.item.lastName}</p>
+          <a className="contactEmail" href={"mailto:" + this.props.item.email}>{this.props.item.email}</a>
+          </li>
       )
-    },
+    }
   });
   
