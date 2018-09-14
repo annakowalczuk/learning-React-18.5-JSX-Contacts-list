ba1bbb84d61ffb5e6e615657e01bfca85e0e3765
Anna Kowalczuk <anna@kowalczuk.eu>
2018-09-14 23:33:52 +0200

    Adjust Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,15 +1,15 @@
 var Contacts = React.createClass({
-    propTypes: {
-      items: React.PropTypes.array.isRequired,
-    },
-  
-    render: function() {
-      var contacts = this.props.items.map(function(contact) {
-          return React.createElement(Contact, {item: contact, key: contact.id});
-      });
-  
-      return (
-        React.createElement('ul', {className: 'contactsList'}, contacts)
-      );
-    }
-  });
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function () {
+
+    return (<ul className="contactsList">
+      function () {
+        this.props.items.map(function (contact) {
+          return ( <Contact item={contact} key={contact.id} />);
+        })}
+    </ul>);
+  }
+});
